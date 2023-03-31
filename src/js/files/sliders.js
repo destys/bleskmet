823cff34f8906261ad12e02586d22dc273da4466
Данил Хальпуков <danil@MacBook-Air-Danil.local>
2023-03-31 14:45:44 +0300

    progress

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -7,7 +7,7 @@
 // Подключаем слайдер Swiper из node_modules
 // При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
 // Пример: { Navigation, Autoplay }
-import Swiper, { Navigation } from 'swiper';
+import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
 /*
 Основниые модули слайдера:
 Navigation, Pagination, Autoplay, 
@@ -17,9 +17,9 @@ EffectFade, Lazy, Manipulation
 
 // Стили Swiper
 // Базовые стили
-import "../../scss/base/swiper.scss";
+// import "../../scss/base/swiper.scss";
 // Полный набор стилей из scss/libs/swiper.scss
-// import "../../scss/libs/swiper.scss";
+import "../../scss/libs/swiper.scss";
 // Полный набор стилей из node_modules
 // import 'swiper/css';
 
@@ -27,15 +27,15 @@ import "../../scss/base/swiper.scss";
 function initSliders() {
 	// Перечень слайдеров
 	// Проверяем, есть ли слайдер на стронице
-	if (document.querySelector('.__slider')) { // Указываем скласс нужного слайдера
+	if (document.querySelector('.intro__slider')) { // Указываем скласс нужного слайдера
 		// Создаем слайдер
-		new Swiper('.__slider', { // Указываем скласс нужного слайдера
+		new Swiper('.intro__slider', { // Указываем скласс нужного слайдера
 			// Подключаем модули слайдера
 			// для конкретного случая 
-			modules: [Navigation],
+			modules: [Autoplay, Navigation, Pagination],
 			observer: true,
 			observeParents: true,
-			slidesPerView: 3,
+			slidesPerView: 1,
 			spaceBetween: 32,
 			autoHeight: true,
 			speed: 800,
@@ -43,38 +43,38 @@ function initSliders() {
 			//touchRatio: 0,
 			//simulateTouch: false,
 			//loop: true,
-			//preloadImages: false,
+			preloadImages: true,
 			//lazy: true,
 
-			/*
+
 			// Эффекты
-			effect: 'fade',
+			// effect: 'fade',
 			autoplay: {
 				delay: 3000,
 				disableOnInteraction: false,
 			},
-			*/
+
 
 			// Пагинация
-			/*
+
 			pagination: {
-				el: '.swiper-pagination',
+				el: '.intro__pagination',
 				clickable: true,
 			},
-			*/
+
 
 			// Скроллбар
-			
+
 			/* scrollbar: {
 				el: '.swiper-scrollbar',
 				draggable: true,
 			}, */
 
 			// Кнопки "влево/вправо"
-			navigation: {
+			/* navigation: {
 				prevEl: '.swiper-button-prev',
 				nextEl: '.swiper-button-next',
-			},
+			}, */
 
 			// Брейкпоинты
 			/*
