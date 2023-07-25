f29dd33d09e66d00e6ce027d0885353e2b7c031b
Данил Хальпуков <danil@MacBook-Air-Danil.local>
2023-07-25 11:39:21 +0300

    progress

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -7,7 +7,7 @@
 // Подключаем слайдер Swiper из node_modules
 // При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
 // Пример: { Navigation, Autoplay }
-import Swiper, { Autoplay, Navigation, Pagination, Thumbs } from 'swiper';
+import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
 /*
 Основниые модули слайдера:
 Navigation, Pagination, Autoplay, 
@@ -136,129 +136,6 @@ function initSliders() {
 
 			pagination: {
 				el: '.pride-about__pagination',
-				clickable: true,
-			},
-
-
-			// Скроллбар
-
-			/* scrollbar: {
-				el: '.swiper-scrollbar',
-				draggable: true,
-			}, */
-
-			// Кнопки "влево/вправо"
-			/* navigation: {
-				prevEl: '.swiper-button-prev',
-				nextEl: '.swiper-button-next',
-			}, */
-
-			// Брейкпоинты
-			/*
-			breakpoints: {
-				320: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
-				},
-				768: {
-					slidesPerView: 2,
-					spaceBetween: 20,
-				},
-				992: {
-					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 4,
-					spaceBetween: 30,
-				},
-			},
-			*/
-			// События
-			on: {
-
-			}
-		});
-	}
-	if (document.querySelector('.product-page__slider') && document.querySelector('.product-page__slider_thumbs')) { // Указываем скласс нужного слайдера
-		// Создаем слайдер
-		const productSliderThumbs = new Swiper('.product-page__slider_thumbs', { // Указываем скласс нужного слайдера
-
-			modules: [Navigation],
-			observer: true,
-			observeParents: true,
-			slidesPerView: 3,
-			spaceBetween: 32,
-			//autoHeight: true,
-			speed: 800,
-
-			//touchRatio: 0,
-			//simulateTouch: false,
-			//loop: true,
-			preloadImages: true,
-			//lazy: true,
-
-
-			// Брейкпоинты
-			/*
-			breakpoints: {
-				320: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
-				},
-				768: {
-					slidesPerView: 2,
-					spaceBetween: 20,
-				},
-				992: {
-					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 4,
-					spaceBetween: 30,
-				},
-			},
-			*/
-			// События
-			on: {
-
-			}
-		});
-		const productSlider = new Swiper('.product-page__slider', { // Указываем скласс нужного слайдера
-
-			modules: [Navigation, Thumbs],
-			observer: true,
-			observeParents: true,
-			slidesPerView: 1,
-			spaceBetween: 32,
-			autoHeight: true,
-			speed: 800,
-			thumbs: {
-				swiper: productSliderThumbs
-			},
-
-			//touchRatio: 0,
-			//simulateTouch: false,
-			//loop: true,
-			preloadImages: true,
-			//lazy: true,
-
-
-			// Эффекты
-			// effect: 'fade',
-			autoplay: {
-				delay: 3000,
-				disableOnInteraction: false,
-			},
-
-
-			// Пагинация
-
-			pagination: {
-				el: '.intro__pagination',
 				clickable: true,
 			},
 
